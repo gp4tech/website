2b761c029ab766b833b215817cd45686f1e2f187
dmeneses <daniela11290@gmail.com>
2019-01-31 09:37:18 -0400

    Added home module and initial routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { LayoutModule } from './layout/layout.module';
     BrowserModule,
     AppRoutingModule,
     LayoutModule,
+    HomeModule,
   ],
   providers: [],
   bootstrap: [
