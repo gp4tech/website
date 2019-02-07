7befbfadcf13169dde3f70e064594def5dc6bf5a
MariaG Garcia Flores <MariaG Garcia@jalasoft.local>
2019-02-07 17:49:29 -0400

    Activity indicators section

---
+++ src/app/home/activity-indicators/activity-indicators.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-indicators',
+  templateUrl: './activity-indicators.component.html',
+  styleUrls: ['./activity-indicators.component.scss']
+})
+export class ActivityIndicatorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home.component';
+import { ActivityIndicatorsComponent } from './activity-indicators/activity-indicators.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ActivityIndicatorsComponent
   ],
   exports: [
     HomeComponent
