96f21ccce5dbfcc3f24d27cc484997e98df024e4
Daniela Meneses <daniela11290@gmail.com>
2019-03-05 14:46:39 -0400

    Footer layout fixed

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -6,4 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  socialLinks = [
+    {
+      faIcon: 'fa-instagram',
+      label: '',
+      link: "https://www.instagram.com/gp4tech",
+    },
+    {
+      faIcon: 'fa-facebook',
+      label: '',
+      link: "https://www.facebook.com/GP4TechJala",
+    },
+    {
+      faIcon: 'fa-twitter',
+      label: '',
+      link: "https://twitter.com/GirlPowerJala",
+    }
+  ];
+
+  infoLinks = [
+    {
+      faIcon: '',
+      label: 'links.who-did-this',
+      link: "#",
+    },
+    {
+      faIcon: '',
+      label: 'links.contact-us',
+      link: "#",
+    },
+  ]
 }
