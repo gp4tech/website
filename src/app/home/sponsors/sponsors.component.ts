fb5e7529503450defded07a917f482e655b67bc0
Giomara Salinas <gsalinas@ecisolutions.com>
2019-03-22 17:53:26 -0400

    removed spaces

--- src/app/home/sponsors/sponsors.component.ts
+++ src/app/home/sponsors/sponsors.component.ts
@@ -12,7 +12,7 @@ export class SponsorsComponent implements OnInit {
     {
       label: 'Fundacion Jala',
       imgPath:
-        'images%2Fsponsors%2Ffundacion-jala-logo.png?alt=media&token=95701108-86b9-46d3-84f8-a1a01d951fb3 ',
+        'images%2Fsponsors%2Ffundacion-jala-logo.png?alt=media&token=95701108-86b9-46d3-84f8-a1a01d951fb3',
       link: 'http://fundacion-jala.org'
     },
     {
