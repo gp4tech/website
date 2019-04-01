315d0492fbf08e50896396cac5849d27e9050398
Daniela Meneses <daniela11290@gmail.com>
2019-03-31 22:23:06 -0400

    Created new functions to send emails via sendgrid

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,8 @@ const urlMetadata = require('url-metadata');
 const cors = require('cors')({
   origin: true
 });
+const sendgrid = require('sendgrid');
+const sendgridClient = sendgrid(functions.config().sendgrid.apikey);
 
 admin.initializeApp(functions.config().firebase);
 
@@ -33,6 +35,16 @@ function updateDocument(collectionName, document) {
 
 function getDocument(collectionName, documentId) {
   return firebaseHelper.firestore.getDocument(db, collectionName, documentId);
+}
+
+function configureEmailBody(body) {
+  var helper = sendgrid.mail;
+  var fromEmail = new helper.Email(body.from);
+  var toEmail = new helper.Email(body.to);
+  var subject = body.subject;
+  var content = new helper.Content('text/html', body.content);
+  var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+  return  mail.toJSON();
 }
 
 exports.updateBlogMetadata = functions.https.onRequest((request, response) => {
@@ -78,3 +90,33 @@ exports.updateBlogViews = functions.https.onRequest((request, response) => {
       .catch(error => response.status(500).send(error));
   });
 });
+
+exports.httpEmail = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    verifyOrigin(req, res);
+
+    if (req.method !== 'POST') {
+      const error = new Error('Only POST requests are accepted');
+      res.status(405).send(error)
+    }
+
+    const request = sendgridClient.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: configureEmailBody(req.body)
+    });
+
+    sendgridClient.API(request)
+      .then((response) => {
+        if (response.body) {
+          return res.status(200).send(response.body);
+        } else {
+          return res.end();
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send(error);
+      });
+  })
+})
