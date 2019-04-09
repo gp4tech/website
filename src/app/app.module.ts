f40e6d809314781c01a4a34e2c33e0e4024aa179
Daniela Meneses <daniela11290@gmail.com>
2019-04-09 11:49:20 -0400

    Created email service and core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from '@angular/fire';
-import {
-  AngularFirestoreModule,
-  FirestoreSettingsToken
-} from '@angular/fire/firestore';
-
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeModule } from './home/home.module';
 import { LayoutModule } from './layout/layout.module';
-import { environment } from 'src/environments/environment';
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
+    CoreModule,
     AppRoutingModule,
     HomeModule,
     LayoutModule,
-    HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    }),
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
   ],
-  providers: [{ provide: FirestoreSettingsToken, useValue: {} }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/core/email.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Email } from '../shared/models/email.model';
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class EmailService {
+
+  emailEndpoint = `${environment.functionsUrl}/httpEmail`;
+  defaultHeaders = new HttpHeaders({ 
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*' 
+  });
+  
+  constructor(private http: HttpClient) { }
+
+  sendEmail(email: Email) {
+    return this.http
+      .post(this.emailEndpoint, email, { headers: this.defaultHeaders })
+  }
+}
