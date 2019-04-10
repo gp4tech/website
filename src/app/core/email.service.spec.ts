e0b487b44cfb4367cd17858551613268c90887cd
Daniela Meneses <daniela11290@gmail.com>
2019-04-09 22:10:02 -0400

    Removed unnecesary extra line

--- src/app/core/email.service.spec.ts
+++ src/app/core/email.service.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-
 import { EmailService } from './email.service';
 
 describe('EmailService', () => {
