f468beb809de71a6f290a63fb24a6043afd65b93
Mauricio Arce <mauricio.arce.mat@gmail.com>
2019-04-12 11:04:03 -0400

    Tests implementation

--- src/app/blog/top-articles/top-article/top-article.component.spec.ts
+++ src/app/blog/top-articles/top-article/top-article.component.spec.ts
@@ -1,20 +1,55 @@
+import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { HttpClientModule } from '@angular/common/http';
+
+import {
+  TranslateModule,
+  TranslateLoader,
+  TranslateFakeLoader
+} from '@ngx-translate/core';
 
 import { TopArticleComponent } from './top-article.component';
+import { ArticlesService } from '../../articles.service';
+import { Article } from '../../../shared/models/article.model';
+
+const angularFirestoreMock = {
+  collection: () => {}
+};
 
 describe('TopArticleComponent', () => {
-  let component: TopArticleComponent;
-  let fixture: ComponentFixture<TopArticleComponent>;
+  @Component({
+    selector: 'gp-top-article-wrapper',
+    template: '<gp-top-article [article]="article"></gp-top-article>'
+  })
+  class TopArticleWrapperComponent {
+    article: Article = {
+      id: '',
+      url: ''
+    };
+  }
+
+  let component: TopArticleWrapperComponent;
+  let fixture: ComponentFixture<TopArticleWrapperComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TopArticleComponent ]
-    })
-    .compileComponents();
+      declarations: [TopArticleWrapperComponent, TopArticleComponent],
+      imports: [
+        HttpClientModule,
+        TranslateModule.forRoot({
+          loader: { provide: TranslateLoader, useClass: TranslateFakeLoader }
+        })
+      ],
+      providers: [
+        ArticlesService,
+        { provide: AngularFirestore, useValue: angularFirestoreMock }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TopArticleComponent);
+    fixture = TestBed.createComponent(TopArticleWrapperComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
