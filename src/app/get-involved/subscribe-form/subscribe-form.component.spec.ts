47a054da9c8dee589f39031b893879ee1b259315
Daniela Meneses <daniela11290@gmail.com>
2019-04-23 23:33:39 -0400

    Fixed tests

--- src/app/get-involved/subscribe-form/subscribe-form.component.spec.ts
+++ src/app/get-involved/subscribe-form/subscribe-form.component.spec.ts
@@ -1,8 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { TranslateModule } from '@ngx-translate/core';
 
 import { SubscribeFormComponent } from './subscribe-form.component';
+import { SharedModule } from '../../shared/shared.module';
 
 describe('SubscribeFormComponent', () => {
   let component: SubscribeFormComponent;
@@ -10,7 +12,11 @@ describe('SubscribeFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [TranslateModule.forRoot()],
+      imports: [
+        ReactiveFormsModule,
+        TranslateModule.forRoot(),
+        SharedModule
+      ],
       declarations: [ SubscribeFormComponent ]
     })
     .compileComponents();
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import {
   TranslateModule,
@@ -53,6 +54,7 @@ describe('HomeComponent', () => {
         TopArticleComponent
       ],
       imports: [
+        RouterTestingModule,
         GetInvolvedModule,
         TranslateModule.forRoot({
           loader: { provide: TranslateLoader, useClass: TranslateFakeLoader }
