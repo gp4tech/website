b5d80ba1a3ba064924f2a44cf4cf982662b75f8e
Lizzy Mendivil <lizzy.mendivil@jalasoft.com>
2019-05-07 20:27:11 -0400

    Create an events component

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
+import { LanguageService } from '../layout/navbar/language-button/language.service';
+import { EventsService } from './events.service';
+import { Events } from '../shared/models/events.model';
+
 @Component({
   selector: 'gp-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.scss']
 })
 export class EventsComponent implements OnInit {
-  constructor() {}
+  events$: Observable<Events[]>;
+  languages = { ES: 'es', EN: 'en' };
+  position = { RIGHT: 'right', LEFT: 'left' };
 
-  ngOnInit() {}
+  constructor(private eventsService: EventsService,
+              public language: LanguageService) {}
+
+  ngOnInit() {
+    this.events$ = this.eventsService.getAll();
+  }
 }
