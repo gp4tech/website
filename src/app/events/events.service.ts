b175096cc5ee3e6be03e6794af18d930ab140c33
Lizzy Mendivil <lizzy.mendivil@jalasoft.com>
2019-05-07 20:26:08 -0400

    Create a service for events

---
+++ src/app/events/events.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+import { DataService } from '../shared/models/data-service.model';
+import { Events } from '../shared/models/events.model';
+import { FirebaseCollections } from '../shared/models/collections.constant';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService extends DataService<Events> {
+  constructor(http: HttpClient, db: AngularFirestore) {
+    super(http, db, FirebaseCollections.events);
+  }
+}
