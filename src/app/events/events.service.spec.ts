7cc1cca2a43e839a9bd38db45a8f7b508f5b9d69
Lizzy Mendivil <lizzy.mendivil@jalasoft.com>
2019-05-14 12:08:34 -0400

    Fix unit tests

--- src/app/events/events.service.spec.ts
+++ src/app/events/events.service.spec.ts
@@ -1,9 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { HttpClientModule } from '@angular/common/http';
 
 import { EventsService } from './events.service';
 
+const angularFirestoreMock = {
+  collection: () => {}
+};
+
 describe('EventsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [
+        EventsService,
+        { provide: AngularFirestore, useValue: angularFirestoreMock }
+      ]
+    })
+  );
 
   it('should be created', () => {
     const service: EventsService = TestBed.get(EventsService);
