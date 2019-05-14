0b2efc4cbbf0ede32c01bf4c52ac2239b445cc6a
Lizzy Mendivil <lizzy.mendivil@jalasoft.com>
2019-05-14 10:05:59 -0400

    Code refactor according code review

--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -4,9 +4,19 @@ import { CommonModule } from '@angular/common';
 import { EventsComponent } from './events.component';
 import { SharedModule } from '../shared/shared.module';
 import { EventsRoutingModule } from './events-routing.module';
+import { EventsService } from './events.service';
 
 @NgModule({
-  declarations: [EventsComponent],
-  imports: [CommonModule, SharedModule, EventsRoutingModule]
+  declarations: [
+    EventsComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    EventsRoutingModule
+  ],
+  providers: [
+    EventsService
+  ]
 })
 export class EventsModule {}
--- src/app/events/events.service.ts
+++ src/app/events/events.service.ts
@@ -6,9 +6,7 @@ import { DataService } from '../shared/models/data-service.model';
 import { Events } from '../shared/models/events.model';
 import { FirebaseCollections } from '../shared/models/collections.constant';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class EventsService extends DataService<Events> {
   constructor(http: HttpClient, db: AngularFirestore) {
     super(http, db, FirebaseCollections.events);
