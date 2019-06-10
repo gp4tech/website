4006c26ea0c3d5ebe65a6f5451d9c8094f36bbd2
Daniela Meneses <daniela11290@gmail.com>
2019-06-09 21:55:13 -0400

    Refactor footer to follow design

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'gp-footer',
@@ -6,6 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+
+  sponsors = [
+    {
+      logoUrl: environment.firebaseStorageUrl + 
+        'images%2Fsponsors%2Ffundacion-jala-logo.png?alt=media&token=95701108-86b9-46d3-84f8-a1a01d951fb3',
+      i18n: 'jala-foundation',
+      link: 'http://fundacion-jala.org/#/es/home',
+    },
+    {
+      logoUrl: environment.firebaseStorageUrl +
+        'images%2Fsponsors%2Fjalasoft-logo.png?alt=media&token=5801be77-50ab-4834-beba-aad687c52311',
+      i18n: 'jala',
+      link: 'http://www.jalasoft.com/',
+    }
+  ];
+
   socialLinks = [
     {
       faIcon: 'fa-instagram',
@@ -23,6 +40,12 @@ export class FooterComponent {
       link: 'https://twitter.com/GirlPowerJala',
     }
   ];
+
+  donation = {
+      faIcon: 'fa-donate',
+      i18n: 'links.donate',
+      link: 'donations',
+  };
 
   infoLinks = [
     {
