8d453a4dac745ab43477101f8f37860292318f77
Lizzy Mendivil <lizzy.mendivil@jalasoft.com>
2019-06-11 13:09:25 -0400

    Fix unit tests

--- src/app/donate/donate.component.spec.ts
+++ src/app/donate/donate.component.spec.ts
@@ -1,25 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
- import { DonateComponent } from './donate.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { of } from 'rxjs';
 
- describe('DonateComponent', () => {
+import { DonateComponent } from './donate.component';
+import { DonateService } from './donate.service';
+import { LanguageService } from '../core/language.service';
+
+const DonateServiceMock = {
+  getAll: () => of([])
+};
+
+describe('DonateComponent', () => {
   let component: DonateComponent;
   let fixture: ComponentFixture<DonateComponent>;
 
-   beforeEach(async(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DonateComponent ]
-    })
-    .compileComponents();
+      imports: [TranslateModule.forRoot()],
+      declarations: [ DonateComponent ],
+      providers: [
+        { provide: DonateService, useValue: DonateServiceMock },
+        LanguageService
+      ]
+    }).compileComponents();
   }));
 
-   beforeEach(() => {
+  beforeEach(() => {
     fixture = TestBed.createComponent(DonateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-   it('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
