7fdbb3bd84dccc994461b6968ceecaea69d0d7c2
Lizzy Mendivil <lizzy.mendivil@jalasoft.com>
2019-06-11 11:40:15 -0400

    Create donate component

---
+++ src/app/donate/donate.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+import { DonateRoutingModule } from './donate-routing.module';
+import { DonateComponent } from './donate.component';
+import { DonateService } from './donate.service';
+
+@NgModule({
+  declarations: [
+    DonateComponent
+  ],
+  imports: [
+    CommonModule,
+    DonateRoutingModule,
+    SharedModule
+  ],
+  providers: [
+    DonateService
+  ]
+})
+export class DonateModule { }
