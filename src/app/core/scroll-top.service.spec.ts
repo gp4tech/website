51a47d6c68dd36671c3c709613d1804812b7ef2a
Mauricio Arce <mauricio.arce.mat@gmail.com>
2019-06-13 19:43:48 -0400

    Fix testing

--- src/app/core/scroll-top.service.spec.ts
+++ src/app/core/scroll-top.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ScrollTopService } from './scroll-top.service';
 
 describe('ScrollTopService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [ScrollTopService]
+    })
+  );
 
   it('should be created', () => {
     const service: ScrollTopService = TestBed.get(ScrollTopService);
--- src/app/shared/directives/transparent-section/transparent-section.directive.spec.ts
+++ src/app/shared/directives/transparent-section/transparent-section.directive.spec.ts
@@ -2,7 +2,7 @@ import { TransparentSectionDirective } from './transparent-section.directive';
 
 describe('TransparentSectionDirective', () => {
   it('should create an instance', () => {
-    const directive = new TransparentSectionDirective(null);
+    const directive = new TransparentSectionDirective(null, null, null);
     expect(directive).toBeTruthy();
   });
 });
