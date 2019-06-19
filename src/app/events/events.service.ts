8d1d5d2c7829663d0380411d01d49af11e041dbb
Lizzy Mendivil <lizzy.mendivil@jalasoft.com>
2019-06-19 12:42:34 -0400

    Fix view events

--- src/app/events/events.service.ts
+++ src/app/events/events.service.ts
@@ -2,13 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AngularFirestore } from '@angular/fire/firestore';
 
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 import { DataService } from '../shared/models/data-service.model';
 import { Event } from '../shared/models/event.model';
 import { FirebaseCollections } from '../shared/models/collections.constant';
+import { DataOrder } from '../shared/models/data-type.model';
 
 @Injectable()
 export class EventsService extends DataService<Event> {
   constructor(http: HttpClient, db: AngularFirestore) {
     super(http, db, FirebaseCollections.events);
+  }
+
+  getActivities(): Observable<Event[]> {
+    return this.getAllSorted('order', DataOrder.asc).pipe(
+      map(activities => {
+        return activities;
+      })
+    );
   }
 }
