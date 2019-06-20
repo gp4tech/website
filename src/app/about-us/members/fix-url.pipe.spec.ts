b0a50a182acd7c7d29c20f850f1116332d1d2170
Daniela Meneses <daniela11290@gmail.com>
2019-06-19 23:20:43 -0400

    Fixed about us issues

---
+++ src/app/about-us/members/fix-url.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FixUrlPipe } from './fix-url.pipe';
+
+describe('FixUrlPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FixUrlPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
