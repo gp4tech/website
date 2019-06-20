dcf79eb7df5a4fcc64d29a1dc54ec54da428c963
Daniela Meneses <daniela11290@gmail.com>
2019-06-20 01:53:30 -0400

    Added auto slide for the carousel element

--- src/app/shared/directives/carousel/carousel.directive.ts
+++ src/app/shared/directives/carousel/carousel.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, AfterViewInit } from '@angular/core';
+import { Directive, ElementRef, AfterViewInit, Input, HostListener } from '@angular/core';
 
 import { Carousel } from 'materialize-css';
 
@@ -6,12 +6,40 @@ import { Carousel } from 'materialize-css';
   selector: '[gpCarousel]'
 })
 export class CarouselDirective implements AfterViewInit {
+  
+  @Input() autoSlide = true;
+  carousel: Carousel;
+  intervalId = null;
+
   constructor(private elementRef: ElementRef) {}
 
   ngAfterViewInit(): void {
-    Carousel.init(this.elementRef.nativeElement, {
+    this.carousel = Carousel.init(this.elementRef.nativeElement, {
       fullWidth: true,
       indicators: true
     });
+
+    this.startAutoSlide();
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.startAutoSlide();
+  }
+
+  @HostListener('click') onClick() {
+    this.stopAutoSlide();
+  }
+
+  startAutoSlide() {
+    if (this.autoSlide && !this.intervalId) {
+      this.intervalId = setInterval(() => {
+        this.carousel.next();
+      }, 8000);
+    }
+  }
+
+  stopAutoSlide() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
   }
 }
