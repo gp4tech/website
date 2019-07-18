844fc68f6559b77b21e244cea3853c2300f4c6b7
Mauricio Arce <mauricio.arce.mat@gmail.com>
2019-07-18 12:42:37 -0400

    Fix end to end tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to website!');
+    expect(page.getTitleText()).toEqual('GIRL POWER FOR TECHNOLOGY');
   });
 
   afterEach(async () => {
