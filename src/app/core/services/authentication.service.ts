f6e90bd163faf4420d64639d8c5402ebbf8938e5
Lizzy Mendivil <lizzy.mendivil@jalasoft.com>
2019-10-09 11:24:59 -0400

    Create auth service for login

---
+++ src/app/core/services/authentication.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  userData: Observable<firebase.User>;
+
+  constructor(private afa: AngularFireAuth, public router: Router) {
+    this.userData = afa.authState;
+  }
+
+  SignIn(email: string, password: string) {
+    this.afa
+      .auth
+      .signInWithEmailAndPassword(email, password)
+      .then((res) => {
+        this.router.navigate(['/']);
+        console.log('Successfully signed in', res);
+    })
+      .catch((error) => {
+        console.log('Something is wrong', error.message);
+    });
+  }
+
+  SignUp(email: string, password: string) {
+    this.afa
+    .auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((res) => {
+      console.log('Successfully signed up', res);
+    })
+    .catch((error) => {
+      console.log('Something is wrong', error.message);
+    });
+  }
+
+  SignOut() {
+    this.afa
+      .auth
+      .signOut();
+  }
+}
