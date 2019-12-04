aacc95efab3d1072c489b39e94992d38aabdcd6c
Monik <monica.apazacora@gmail.com>
2019-12-04 09:11:11 -0300

    mission-vision component format fixed

--- src/app/about-us/mission-vision/mission-vision.component.spec.ts
+++ src/app/about-us/mission-vision/mission-vision.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { TranslateModule, TranslateLoader, TranslateFakeLoader } from '@ngx-translate/core';
+
 import { MissionVisionComponent } from './mission-vision.component';
 
 describe('MissionVisionComponent', () => {
